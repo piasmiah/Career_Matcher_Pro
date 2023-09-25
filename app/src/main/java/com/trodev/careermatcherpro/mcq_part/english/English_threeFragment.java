d4df9cf1bda664464859a2d4bb126d31c78f7488
jubayer282 <jubayerhossain235600@gmail.com>
2023-09-25 10:04:13 +0600

    Completing English MCQ fragement and also writedown five fragement mcq question

--- app/src/main/java/com/trodev/careermatcherpro/mcq_part/english/English_threeFragment.java
+++ app/src/main/java/com/trodev/careermatcherpro/mcq_part/english/English_threeFragment.java
@@ -1,66 +1,157 @@
 package com.trodev.careermatcherpro.mcq_part.english;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.trodev.careermatcherpro.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link English_threeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class English_threeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String json_url = "https://zobayer-dev-e12aa.web.app/english_mcq.json";
+    RecyclerView recyclerView;
+    List<EnglishModel> list;
+    ProgressBar progressBar;
 
     public English_threeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment English_threeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static English_threeFragment newInstance(String param1, String param2) {
-        English_threeFragment fragment = new English_threeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_english_three, container, false);
+        View view = inflater.inflate(R.layout.fragment_english_three, container, false);
+
+        /*init views*/
+        recyclerView = view.findViewById(R.id.dataRv);
+        progressBar = view.findViewById(R.id.progress_bar);
+
+        progressBar.setVisibility(View.VISIBLE);
+        list = new ArrayList<>();
+
+        loadData();
+
+        return view;
     }
+
+    private void loadData() {
+
+        /*which fragments load data init it*/
+        English_threeFragment.GetData getData = new English_threeFragment.GetData();
+        getData.execute();
+
+    }
+
+
+    public class GetData extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String current = "";
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+
+                url = new URL(json_url);
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+
+                while (data != -1) {
+                    current += (char) data;
+                    data = inputStreamReader.read();
+                }
+
+                return current;
+            } catch (Exception e) {
+
+            } finally {
+                if (urlConnection != null) {
+
+                    urlConnection.disconnect();
+                }
+            }
+            return current;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            try {
+
+                progressBar.setVisibility(View.INVISIBLE);
+
+                JSONObject jsonObject = new JSONObject(s);
+
+                // database info
+                JSONArray jsonArray = jsonObject.getJSONArray("english_third");
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+
+                    JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+
+                    /*create model class variable, object*/
+                    EnglishModel modelClass = new EnglishModel();
+
+                    /*set data on recyclerview*/
+                    modelClass.setMcq_no(jsonObject1.getString("mcq_no"));
+
+                    modelClass.setGrp_ques(jsonObject1.getString("grp_ques"));
+
+                    modelClass.setFirst(jsonObject1.getString("first"));
+
+                    modelClass.setSecond(jsonObject1.getString("second"));
+
+                    modelClass.setThird(jsonObject1.getString("third"));
+
+                    modelClass.setFourth(jsonObject1.getString("fourth"));
+
+                    modelClass.setAns(jsonObject1.getString("ans"));
+
+
+                    /*add model data on empty model class*/
+                    list.add(modelClass);
+                }
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            PutDataIntoRecyclerview(list);
+        }
+    }
+
+    private void PutDataIntoRecyclerview(List<EnglishModel> list) {
+
+        progressBar.setVisibility(View.INVISIBLE);
+        EnglishAdapter customAdapter = new EnglishAdapter(getContext(), list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(customAdapter);
+
+    }
+
 }
